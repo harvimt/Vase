1aa8ee024ff9cf445c9667b2388b489f68f5c94e
Mark Harviston <Mark.Harviston.-ND@disney.com>
2015-04-14 15:21:34 -0700

    move sockjstest/sockjs.py -> tests/test_wsecho.py

--- tests/test_wsecho.py
+++ tests/test_wsecho.py
@@ -1,12 +1,99 @@
 from multiprocessing import Process
 import pytest
 import time
-import selenium
+from vase import Vase
+from vase.sockjs import forbid_websocket
+
+app = Vase(__name__)
+
+
+@app.route(path='/')
+def home(request):
+    return '<html><script src="//cdn.jsdelivr.net/sockjs/0.3.4/sockjs.min.js"></script><div id="msg"></div>'
+
+
+@app.endpoint(path="/echo/sockjs")
+class EchoEndpoint:
+    """
+    WebSocket endpoint
+    Has the following attributes:
+    `bag` - a dictionary that is shared between all instances of this endpoint
+    `transport` - used to send messages into the websocket
+    """
+    def on_connect(self):
+        print("Successfully connected")
+
+    def on_message(self, message):
+        print("on_message", self, message)
+        self.transport.send(message)
+
+    def on_close(self, exc=None):
+        print("Connection closed")
+
+
+@app.endpoint(path="/echo/nosockjs", with_sockjs=False)
+class EchoEndpointNoSockJS:
+    """
+    WebSocket endpoint
+    Has the following attributes:
+    `bag` - a dictionary that is shared between all instances of this endpoint
+    `transport` - used to send messages into the websocket
+    """
+    def on_connect(self):
+        print("Successfully connected")
+
+    def on_message(self, message):
+        print("on_message", self, message)
+        self.transport.send(message)
+
+    def on_close(self, exc=None):
+        print("Connection closed")
+
+
+@app.endpoint(path="/disabled_websocket_echo")
+@forbid_websocket
+class EchoEndpointNoWS:
+    """
+    WebSocket endpoint
+    Has the following attributes:
+    `bag` - a dictionary that is shared between all instances of this endpoint
+    `transport` - used to send messages into the websocket
+    """
+    def on_connect(self):
+        print("Successfully connected")
+
+    def on_message(self, message):
+        self.transport.send(message)
+
+    def on_close(self, exc=None):
+        print("Connection closed")
+
+
+@app.endpoint(path="/close")
+class EchoEndpointClose:
+    """
+    WebSocket endpoint
+    Has the following attributes:
+    `bag` - a dictionary that is shared between all instances of this endpoint
+    `transport` - used to send messages into the websocket
+    """
+    def on_connect(self):
+        print("Connected")
+        self.transport.close()
+
+    def on_message(self, message):
+        pass
+
+    def on_close(self, exc=None):
+        pass
+
+
+if __name__ == '__main__':
+    app.run()
 
 
 @pytest.fixture(scope='session')
 def wsecho(request):
-    from sockjstest.sockjs import app
     proc = Process(target=app.run)
     proc.start()
     request.addfinalizer(proc.terminate)
